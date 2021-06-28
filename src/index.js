8745207bdb362230b2ec56b33b00a373b4486ec0
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-06-28 23:03:52 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
 import fetchCountries from './fetchCountries.js';
 import countryCard from './template.hbs';
 import countryListHbs from './countryList.hbs';
-import { alert, defaultModules } from '@pnotify/core';
-import debounce from 'lodash';
+var debounce = require('lodash.debounce');
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import { error } from '@pnotify/core';
 
 const searchInputRef = document.querySelector('input');
 const countryInfoEl = document.querySelector('.wrapper');
 const countryListRef = document.querySelector('.country-list');
 
-searchInputRef.addEventListener('input', e => {
-  debounce(() => {
-    cosole.log(e.target.value);
-  }, 2000);
-});
-
-function renderCountryList(counties) {
-  countryListRef.insertAdjacentHTML('beforeend', countryListHbs(counties));
-}
+searchInputRef.addEventListener('input', debounce(searchCountru, 500));
 
 function renderCountry(country) {
-  countryInfoEl.insertAdjacentHTML('beforeend', countryCard(country));
+  if (country.length === 1) {
+    countryInfoEl.insertAdjacentHTML('beforeend', countryCard(country));
+  } else if (country.length >= 2 && country.length <= 10) {
+    countryListRef.insertAdjacentHTML('beforeend', countryListHbs(country));
+  } else if (country.length > 10) {
+    throwError('to many maches');
+  }
 }
 
-function event(e) {
-  let country = e.target.value;
+function throwError(err) {
+  error({ text: `${err}` });
+}
 
-  fetchCountries(country).then(data => {
-    data.find(contry => {
-      renderCountry(contry);
-    });
-  });
-  //   fetchCountries(country).then(renderCountryList).then(renderCountry);
+function searchCountru(e) {
+  e.preventDefault();
+
+  const countryQuery = searchInputRef.value;
+
+  if (countryQuery.length === 0) {
+    searchInputRef.innerHTML = '';
+    countryInfoEl.innerHTML = '';
+    countryListRef.innerHTML = '';
+
+    return;
+  }
+
+  fetchCountries(countryQuery)
+    .then(renderCountry)
+    .catch(error => throwError('not found'));
 }
